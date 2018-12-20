b5186b7fca83eec6c2af75c5e5692e51df2429d9
Patrick Diehl <diehlpk@centaur.nic.uoregon.edu>
2018-12-20 12:51:48 -0800

    make sure that boost uses the correct gcc

--- build-boost.sh
+++ build-boost.sh
@@ -3,10 +3,11 @@ set -x
 
 . source-gcc.sh
 
-if [ ! -d "boost_1_68_0/" ]; then
-    wget 'http://downloads.sourceforge.net/project/boost/boost/1.68.0/boost_1_68_0.tar.bz2'
-    tar xf boost_1_68_0.tar.bz2
+#if [ ! -d "boost_1_68_0/" ]; then
+#    wget 'http://downloads.sourceforge.net/project/boost/boost/1.68.0/boost_1_68_0.tar.bz2'
+#    tar xf boost_1_68_0.tar.bz2
     cd boost_1_68_0
-    ./bootstrap.sh --prefix=$HOME/opt/boost --with-toolset=gcc
+    echo "using gcc : 8.2 : $CXX ; " >> tools/build/src/user-config.jam
+    ./bootstrap.sh --prefix=$HOME/opt/boost --with-toolset=gcc 
     ./b2 -j 20 install --with-atomic --with-filesystem --with-program_options --with-regex --with-system --with-chrono --with-date_time --with-thread
-fi
+#fi
