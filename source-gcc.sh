51b38052ce5793457427c9c5e55f22827524653c
Patrick Diehl <patrickdiehl@lsu.edu>
2019-01-15 18:45:10 -0800

    Make the CXXFLAGS depending on the arch

--- source-gcc.sh
+++ source-gcc.sh
@@ -2,8 +2,15 @@ export CC=$HOME/opt/gcc/bin/gcc
 export CXX=$HOME/opt/gcc/bin/g++
 export LD_LIBRARY_PATH=$HOME/opt/gcc/lib64
 
-
+arch=$(uname -i)
 
 export CFLAGS=-fPIC
-export CXXFLAGS="-fPIC -mcpu=native -mtune=native  -ffast-math -std=c++14 "
 export LDCXXFLAGS="$LDFLAGS -std=c++14 "
+
+if [ "$arch" == 'ppc64le' ];
+export CXXFLAGS="-fPIC -mcpu=native -mtune=native  -ffast-math -std=c++14 "
+fi
+
+if [ "$arch" == 'x86_64' ];
+export CXXFLAGS="-fPIC -march=native  -ffast-math -std=c++14 "
+fi
