36659b732cfa5a98666cbe477fe7edf439966235
Parsa Amini <parsa@cct.lsu.edu>
2019-01-22 17:18:11 -0600

    Don't use CMake to build CMake

--- build-cmake.sh
+++ build-cmake.sh
@@ -2,20 +2,18 @@
 set -x
 set -e
 
-if [ -z ${octotiger_source_me_sources} ] ; then
+CMAKE_VERSION=3.13.2
+if [ -z ${octotiger_source_me_sources} ]; then
 	. source-me.sh
 fi
 
-
-if [ ! -d "cmake-3.13.2/" ]; then
-   wget https://github.com/Kitware/CMake/releases/download/v3.13.2/cmake-3.13.2.tar.gz
-   tar -xf cmake-3.13.2.tar.gz 
+if [ ! -d "cmake-${CMAKE_VERSION}/" ]; then
+   curl -JL https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}.tar.gz | tar xz
 fi
-cd cmake-3.13.2
+cd cmake-${CMAKE_VERSION}
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=$HOME/opt/cmake ..
-make -j${PARALLEL_BUILD}
+./bootstrap --parallel=${PARALLEL_BUILD} --prefix=$HOME/opt/cmake
 make install
 
 
