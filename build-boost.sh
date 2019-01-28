333d77abea294d1c34da9c84665f64327a01a3bd
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2019-01-28 18:35:19 +0100

    Fixed build boost with new folder scheme

--- build-boost.sh
+++ build-boost.sh
@@ -12,9 +12,13 @@ cd $SOURCE_ROOT
 if [ ! -f "boost_$BOOST_SUFFIX.tar.bz2" ]; then
     wget http://downloads.sourceforge.net/project/boost/boost/$BOOST_VERSION/boost_$BOOST_SUFFIX.tar.bz2
 fi
+if [ -d "boost_$BOOST_SUFFIX" ]; then
+    rm -rf boost_$BOOST_SUFFIX
+fi
 tar xf boost_$BOOST_SUFFIX.tar.bz2
 cd boost_$BOOST_SUFFIX
 echo "using gcc : 8.2 : $CXX ; " >> tools/build/src/user-config.jam
 ./bootstrap.sh --prefix=$BOOST_ROOT --with-toolset=gcc
-./b2 -j${PARALLEL_BUILD} install --with-atomic --with-filesystem --with-program_options --with-regex --with-system --with-chrono --with-date_time --with-thread cd ..
+./b2 -j${PARALLEL_BUILD} install --with-atomic --with-filesystem --with-program_options --with-regex --with-system --with-chrono --with-date_time --with-thread 
+cd $BUILD_ROOT
 cp sign.hpp $BOOST_ROOT/include/boost/spirit/home/support/detail/
