2c87a56bd645c3c24e1c08617455a67f2431f226
Parsa Amini <me@parsaamini.net>
2019-02-16 22:33:05 -0600

    Clean up source-me.sh

--- source-me.sh
+++ source-me.sh
@@ -31,36 +31,39 @@ export octotiger_source_me_sources=1
 ################################################################################
 # Host-specific configuration
 ################################################################################
-hostid=$(hostname)
-if [[ ${hostid} == krypton ]]; then
-    echo "compiling for krypton, doing additional setup";
-    module load cuda-9.2
-elif echo $hostid | grep -Fxq argon-tesla1; then
-    echo "compiling for argon-tesla1, doing additional setup";
-    source /usr/local.nfs/Modules/init/bash
-    module load cuda-9.0
-    export CUDATOOLKIT_HOME=/usr/local.nfs/sw/cuda/cuda-9.0
-    export CUDAFLAGS="--cuda-path=$CUDATOOLKIT_HOME \
+case $(hostname) in
+    krypton)
+        echo 'Compiling for krypton, doing additional setup';
+        module load cuda-9.2
+        ;;
+    *argon-tesla1*)
+        echo 'Compiling for argon-tesla1, doing additional setup';
+        source /usr/local.nfs/Modules/init/bash
+        module load cuda-9.0
+        export CUDATOOLKIT_HOME=/usr/local.nfs/sw/cuda/cuda-9.0
+        export CUDAFLAGS="--cuda-path=$CUDATOOLKIT_HOME \
  -L$CUDATOOLKIT_HOME/lib64 \
  -L$CUDATOOLKIT_HOME/extras/CUPTI/lib64"
-    export CUDA_VISIBLE_DEVICES=0,1
-    export LD_LIBRARY_PATH=/usr/local.nfs/sw/cuda/cuda-9.0/lib64:$LD_LIBRARY_PATH
-elif echo $hostid | grep -Fxq argon-tesla2; then
-    echo "compiling for argon-tesla2, doing additional setup";
-    source /usr/local.nfs/Modules/init/bash
-    module load cuda-9.0
-    export CUDATOOLKIT_HOME=/usr/local.nfs/sw/cuda/cuda-9.0
-    export CUDAFLAGS="--cuda-path=$CUDATOOLKIT_HOME \
+        export CUDA_VISIBLE_DEVICES=0,1
+        export LD_LIBRARY_PATH=/usr/local.nfs/sw/cuda/cuda-9.0/lib64:$LD_LIBRARY_PATH
+        ;;
+    *argon-tesla2*)
+        echo 'Compiling for argon-tesla2, doing additional setup';
+        source /usr/local.nfs/Modules/init/bash
+        module load cuda-9.0
+        export CUDATOOLKIT_HOME=/usr/local.nfs/sw/cuda/cuda-9.0
+        export CUDAFLAGS="--cuda-path=$CUDATOOLKIT_HOME \
  -L$CUDATOOLKIT_HOME/lib64 \
  -L$CUDATOOLKIT_HOME/extras/CUPTI/lib64 \
  -lcudart_static -ldl -lrt -pthread \
  -lcuda -lcublas "
-    export CUDA_VISIBLE_DEVICES=0,1
-else
-    echo "compiling for normal desktop machine, expecting cuda in /usr/local/cuda";
-    export CUDAFLAGS=""
-fi
-
+        export CUDA_VISIBLE_DEVICES=0,1
+        ;;
+    *)
+        echo 'Compiling for a generic machine, expecting CUDA in "/usr/local/cuda"';
+        export CUDAFLAGS=""
+        ;;
+esac
 
 ################################################################################
 # Command-line help
