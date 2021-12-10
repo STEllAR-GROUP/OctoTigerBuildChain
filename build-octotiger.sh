f91aff1725001c64f0de46182025635c400acdb3
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2021-12-10 17:11:25 +0100

    Updated build for octotiger master

--- build-octotiger.sh
+++ build-octotiger.sh
@@ -13,7 +13,8 @@ DIR_BUILD=${INSTALL_ROOT}/octotiger/build
 if [[ ! -d ${DIR_SRC} ]]; then
     git clone https://github.com/STEllAR-GROUP/octotiger.git ${DIR_SRC}
     pushd ${DIR_SRC}
-    git checkout daint_production_build
+    #git checkout daint_production_build
+    git checkout master
     git submodule update --init --recursive
     popd
 fi
