87050970e896206ba6522b34ab67ac88bcc80823
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2022-01-28 20:49:26 +0100

    Add functional

--- build-boost.sh
+++ build-boost.sh
@@ -58,6 +58,7 @@ if [[ ! -d ${DIR_SRC} ]]; then
 	git submodule update --init --recursive libs/mpl/
 	git submodule update --init --recursive libs/preprocessor/
 	git submodule update --init --recursive libs/detail/
+	git submodule update --init --recursive libs/functional/
 	# checkout actual compoments that we want
 	git submodule update --init --recursive libs/thread/
 	git submodule update --init --recursive libs/iostreams/
