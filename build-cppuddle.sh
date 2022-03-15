f3f806f498d0d3f55fc46807bc06aa8d33a742cf
Gregor Daiss <Gregor.Daiss+git@gmail.com>
2022-03-15 17:52:11 -0500

    Build with correct cppuddle version

--- build-cppuddle.sh
+++ build-cppuddle.sh
@@ -11,13 +11,30 @@ mkdir -p ${DIR_BUILD}
 
 if [[ ! -d ${DIR_SRC} ]]; then
     git clone https://github.com/SC-SGS/CPPuddle.git ${DIR_SRC}
+    cd ${DIR_SRC}
+    git checkout work_aggregation_experimental
+    cd -
 fi
 
 cd ${DIR_SRC}
 git pull
 cd -
 
-${CMAKE_COMMAND} -H${DIR_SRC} -B${DIR_BUILD} -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=${DIR_INSTALL}/cppuddle -DCPPUDDLE_WITH_TESTS=OFF -DCPPUDDLE_WITH_COUNTERS=OFF
+${CMAKE_COMMAND} \
+    -H${DIR_SRC} \
+    -B${DIR_BUILD} \
+    -DCMAKE_CXX_COMPILER=$CXX \
+    -DCMAKE_BUILD_TYPE=${BUILD_TYPE} \
+    -DCMAKE_INSTALL_PREFIX=${DIR_INSTALL}/cppuddle \
+    -DCPPUDDLE_WITH_TESTS=ON \
+    -DCPPUDDLE_WITH_COUNTERS=ON \
+    -DCPPUDDLE_WITH_HPX=ON \
+    -DCPPUDDLE_WITH_CUDA=ON \
+    -DCPPUDDLE_WITH_KOKKOS=ON \
+    -DHPX_DIR=$INSTALL_ROOT/hpx/${LIB_DIR_NAME}/cmake/HPX/ \
+    -DKokkos_DIR=$INSTALL_ROOT/kokkos/install/${LIB_DIR_NAME}/cmake/Kokkos \
+    -DHPXKokkos_DIR=$INSTALL_ROOT/hpx-kokkos/install/${LIB_DIR_NAME}/cmake/HPXKokkos
+
 ${CMAKE_COMMAND} --build ${DIR_BUILD} -- -j${PARALLEL_BUILD} VERBOSE=1
 ${CMAKE_COMMAND} --build ${DIR_BUILD} --target install
 
