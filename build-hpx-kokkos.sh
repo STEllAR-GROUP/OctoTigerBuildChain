0cd1bd9817137b438818df78e4ef1721fd787b71
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2022-05-17 12:41:49 -0500

    Revert "Add hpx kokkos patch"
    This reverts commit 3bfabe08760b04b52923abd72e158b4185bcbbcf.

--- build-hpx-kokkos.sh
+++ build-hpx-kokkos.sh
@@ -12,6 +12,7 @@ DIR_SRC=${SOURCE_ROOT}/hpx-kokkos
 DIR_BUILD=${INSTALL_ROOT}/hpx-kokkos/build
 DIR_INSTALL=${INSTALL_ROOT}/hpx-kokkos
 
+rm -rf "${DIR_SRC}"
 if [[ ! -d ${DIR_SRC} ]]; then
     (
         mkdir -p ${DIR_SRC}
@@ -21,8 +22,6 @@ if [[ ! -d ${DIR_SRC} ]]; then
 	cd hpx-kokkos
 	#git checkout ce63b0630c739a4df899749ed67baa867862f3fc
 	git checkout ${HPX_KOKKOS_VERSION}
-	cp ../../add_future.patch .
-	git apply add_future.patch
 	cd ..
     )
 fi
